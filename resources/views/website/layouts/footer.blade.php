4b268291d608bf1ea11f401bdb723a03edde5b3a
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2023-08-05 17:33:38 +0300

    F

--- resources/views/website/layouts/footer.blade.php
+++ resources/views/website/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <!-- footer -->
 <footer style="direction: rtl;">
   <div class="footer">
-    <div><a href="/index.html" class="hidden lg:flex text-4xl uppercase  flex-col items-center  ">
-      <h2>زم<span class="text-[#7E918C]">ن</span></h2>
-      <h2><span class="text-[#7E918C]">za</span>man</h2>
+    <div><a href="{{route('home')}}" class="hidden lg:flex text-4xl uppercase  flex-col items-center  ">
+     <img src="{{ asset('website/image/logo.png') }}" width="120">
+
     </a>
     <p>مرخص من الهيئة العامة للعقار</p>
     </div>
@@ -13,12 +13,8 @@
     <div>
       <ul>
         <h1>نوع</h1>
-        <li><a href="">عقار السعودية | عقارات للبيع في السعودية</a></li>
         <li><a href="">مخططات للبيع في المملكة</a></li>
-        <li><a href="">عقار الرياض | عقارات للبيع في الرياض</a></li>
-        <li><a href="">عقار جدة | عقارات للبيع في جدة</a></li>
-        <li><a href="">عقار مكة | عقارات للبيع في مكة</a></li>
-        <li><a href="">عقار المدينة | عقارات للبيع في المدينة المنورة</a></li>
+
       </ul>
     </div>
 
@@ -28,10 +24,7 @@
       <ul>
           <h1>المناطق</h1>
       <li><a href=""> عقارات للبيع في شمال الرياض </a></li>
-      <li><a href="">عقارات للبيع في شرق الرياض</a></li>
-      <li><a href="">عقارات للبيع في وسط الرياض</a></li>
-      <li><a href="">عقارات للبيع في جنوب الرياض</a></li>
-      <li><a href="">عقارات للبيع في غرب الرياض</a></li>
+
 
       </ul>
     </div>
