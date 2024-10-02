8a078b6c1c22b31390655a8485e4140b4c7a08fa
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2024-10-02 21:52:10 +0300

    tests

--- resources/views/auctions/delete_modal.blade.php
+++ resources/views/auctions/delete_modal.blade.php
@@ -1,18 +1,18 @@
 <!-- Delete Banner Modal -->
-<div class="modal fade" id="bannerDeleteModal{{$user->id}}" tabindex="-1" aria-labelledby="bannerDeleteModalLabel{{$user->id}}" aria-hidden="true">
+<div class="modal fade" id="bannerDeleteModal{{$auction->id}}" tabindex="-1" aria-labelledby="bannerDeleteModalLabel{{$auction->id}}" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="bannerDeleteModalLabel{{$user->id}}">@lang('lang.Delete Banner')</h5>
+                <h5 class="modal-title" id="bannerDeleteModalLabel{{$auction->id}}">@lang('lang.Delete Banner')</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p>@lang('lang.Are you sure you want to delete this banner?')</p>
-                <p><strong>{{ $user->name }}</strong></p>
+                <p><strong>{{ $auction->name }}</strong></p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">@lang('lang.Close')</button>
-                <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline;">
+                <form action="{{ route('auctions.destroy', $auction->id) }}" method="POST" style="display: inline;">
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="btn btn-danger">@lang('lang.Delete')</button>
--- resources/views/auctions/index.blade.php
+++ resources/views/auctions/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('head-content')
-    @include('partials.title-meta', ['title' => 'Users'])
+    @include('partials.title-meta', ['title' => 'Auctions'])
     @include('partials.head-css')
     <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
 @endsection
@@ -13,65 +13,117 @@
 
                 <div class="row">
                     <div class="col-12">
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bannerAddModal">
-                            {{-- @lang('lang.Add User') --}}
-                        </button>
+                        {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bannerAddModal">
+                            @lang('lang.Add Auction')
+                        </button> --}}
                         <br>
 
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title">@lang('lang.Users')</h4>
+                                <h4 class="card-title">@lang('lang.Auctions')</h4>
                                 <div class="table-responsive">
                                     <table id="datatable" class="table table-bordered" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                         <thead>
                                             <tr>
                                                 <th>@lang('lang.ID')</th>
-                                                <th>@lang('lang.Name')</th>
-                                                <th>@lang('lang.Email')</th>
-                                                <th>@lang('lang.Account Type')</th>
-                                                <th>@lang('lang.Is Active')</th>
+                                                <th>@lang('lang.Type')</th>
+                                                <th>@lang('lang.Price')</th>
                                                 <th>@lang('lang.Phone')</th>
-                                                <th>@lang('lang.License')</th>
-                                                <th>@lang('lang.Commercial Registration')</th>
-                                                <th>@lang('lang.City ID')</th>
-                                                <th>@lang('lang.Profile Image')</th>
+                                                <th>@lang('lang.Number of Rooms')</th>
+                                                <th>@lang('lang.Bathrooms')</th>
+                                                <th>@lang('lang.Hall Number')</th>
+                                                <th>@lang('lang.Council Rooms')</th>
+                                                <th>@lang('lang.Land Area')</th>
+                                                <th>@lang('lang.Street')</th>
+                                                <th>@lang('lang.Number of Streets')</th>
+                                                <th>@lang('lang.Street Area')</th>
+                                                <th>@lang('lang.Street Facing')</th>
+                                                <th>@lang('lang.Electricity')</th>
+                                                <th>@lang('lang.Water')</th>
+                                                <th>@lang('lang.Depth')</th>
+                                                <th>@lang('lang.Features and Facilities')</th>
+                                                <th>@lang('lang.Description')</th>
+                                                <th>@lang('lang.Real Estate Characteristics')</th>
+                                                <th>@lang('lang.WhatsApp')</th>
+                                                <th>@lang('lang.Email')</th>
+                                                <th>@lang('lang.Marketer Name')</th>
+                                                <th>@lang('lang.License Number')</th>
+                                                <th>@lang('lang.City')</th>
+                                                <th>@lang('lang.Category')</th>
+                                                <th>@lang('lang.Category ID')</th>
+                                                <th>@lang('lang.Images')</th>
+                                                <th>@lang('lang.Plan')</th>
+                                                <th>@lang('lang.Videos')</th>
                                                 <th>@lang('lang.Actions')</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach ($users as $user)
+                                            @foreach ($auctions as $auction)
                                                 <tr>
-                                                    <td>{{ $user->id }}</td>
-                                                    <td>{{ $user->name }}</td>
-                                                    <td>{{ $user->email }}</td>
-                                                    <td>{{ $user->account_type }}</td>
-                                                    <td>{{ $user->is_active ? 'Active' : 'Inactive' }}</td>
-                                                    <td>{{ $user->phone }}</td>
-                                                    <td>{{ $user->val_license }}</td>
-                                                    <td>{{ $user->commercial_registration_number }}</td>
-                                                    <td>{{ $user->city_id }}</td>
+                                                    <td>{{ $auction->id }}</td>
+                                                    <td>{{ $auction->type }}</td>
+                                                    <td>{{ $auction->price }}</td>
+                                                    <td>{{ $auction->phone }}</td>
+                                                    <td>{{ $auction->number_of_rooms }}</td>
+                                                    <td>{{ $auction->bathrooms_of_rooms }}</td>
+                                                    <td>{{ $auction->hall_number }}</td>
+                                                    <td>{{ $auction->number_of_council_rooms }}</td>
+                                                    <td>{{ $auction->land_area }}</td>
+                                                    <td>{{ $auction->street }}</td>
+                                                    <td>{{ $auction->number_of_streets }}</td>
+                                                    <td>{{ $auction->street_area }}</td>
+                                                    <td>{{ $auction->street_facing }}</td>
+                                                    <td>{{ $auction->electricity }}</td>
+                                                    <td>{{ $auction->water }}</td>
+                                                    <td>{{ $auction->depth }}</td>
+                                                    <td>{{ $auction->features_facilities }}</td>
+                                                    <td>{{ $auction->description }}</td>
+                                                    <td>{{ $auction->real_estate_characteristics }}</td>
+                                                    <td>{{ $auction->whatsup }}</td>
+                                                    <td>{{ $auction->email }}</td>
+                                                    <td>{{ $auction->marketer_name }}</td>
+                                                    <td>{{ $auction->license_number }}</td>
+                                                    <td>{{ optional($auction->city)->name ?? 'N/A' }}</td> <!-- Assuming 'name' is a field in the cities table -->
+                                                    <td>{{ optional($auction->category)->name ?? 'N/A' }}</td> <!-- Assuming 'name' is a field in the categories table -->
                                                     <td>
-                                                        @if($user->profileImage)
-                                                            <img src="{{ asset('storage/' . $user->profileImage) }}" alt="Profile Image" style="width: 50px; height: 50px; object-fit: cover;">
+                                                        @foreach ($auction->images as $image)
+                                                            <img src="{{ $image }}" alt="Auction Image" style="width: 50px; height: 50px; object-fit: cover; margin: 2px;">
+                                                        @endforeach
+                                                    </td>
+                                                    <td>
+                                                        @if ($auction->plan)
+                                                            <img src="{{ $auction->plan }}" alt="Plan" style="width: 50px; height: 50px; object-fit: cover;">
                                                         @else
-                                                            <span>No Image</span>
+                                                            <span>No Plan</span>
                                                         @endif
                                                     </td>
                                                     <td>
+                                                        @foreach ($auction->videos as $video)
+                                                            <a href="{{ $video }}" target="_blank">View Video</a>
+                                                        @endforeach
+                                                    </td>
+                                                    <td>
                                                         <div class="d-flex flex-column flex-md-row" style="gap: 10px;">
-                                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#bannerDeleteModal{{ $user->id }}">
+                                                            {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bannerEditModal{{ $auction->id }}">
+                                                                @lang('lang.Edit')
+                                                            </button> --}}
+
+                                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#bannerDeleteModal{{ $auction->id }}">
                                                                 @lang('lang.Delete')
                                                             </button>
                                                         </div>
                                                     </td>
                                                 </tr>
 
-                                                @include('users.delete_modal', ['user' => $user])
+                                                @include('auctions.edit_modal', ['auction' => $auction])
+                                                @include('auctions.delete_modal', ['auction' => $auction])
                                             @endforeach
 
+                                            @include('auctions.add_modal')
                                         </tbody>
                                     </table>
                                 </div>
+
                             </div>
                         </div>
                     </div> <!-- end col -->
@@ -88,5 +140,18 @@
         $(document).ready(function() {
             $('#datatable').DataTable();
         });
+    </script>
+    <script>
+        function previewImage(event, bannerId = null) {
+            var reader = new FileReader();
+            reader.onload = function() {
+                // Determine which image preview element to update
+                var outputId = bannerId ? 'imagePreview' + bannerId : 'imagePreview';
+                var output = document.getElementById(outputId);
+                output.src = reader.result;
+                output.style.display = 'block'; // Show the image when file is selected
+            }
+            reader.readAsDataURL(event.target.files[0]);
+        }
     </script>
 @endpush
