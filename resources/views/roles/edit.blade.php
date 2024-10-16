1d20988c3567773854ee2dfe847753e065309591
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2024-10-16 19:33:21 +0300

    roles

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('head-content')
-    @include('partials.title-meta', ['title' => __('roles.title')])
+    @include('partials.title-meta', ['title' => __('lang.title')])
     @include('partials.head-css')
 @endsection
 
@@ -15,35 +15,37 @@
                         <div class="card-body">
                             <h5 class="card-title">{{ __('lang.permissions') }}</h5>
                             <p class="card-title-desc">{{ __('lang.permissions') }}</p>
-                            <form class="form-horizontal" action="{{ route('roles.update', $role->id) }}" method="post" enctype="multipart/form-data">
+                            <form class="form-horizontal" action="{{ route('roles.update',$role->id) }}" method="post" enctype="multipart/form-data">
                                 @method('put')
                                 @csrf
                                 <fieldset class="content-group">
                                     <legend class="text-bold"></legend>
                                     <div class="col-lg-12">
-
                                         <div class="col-lg-12">
-                                            <div class="form-group ">
+                                            <div class="form-group">
                                                 <label for="profile-ar[title]" class="control-label col-lg-2">{{ __('lang.permission_name') }}</label>
                                                 <div class="col-lg-10" id="cp2" style="border: none">
                                                     <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                                                        type="text" name="name" value="{{ $role->name }}"
-                                                        required>
+                                                        type="text" name="name" required>
                                                 </div>
                                             </div>
                                         </div>
 
                                         <div class="col-lg-12">
-                                            <div class="form-group ">
+                                            <div class="form-group">
                                                 <label for="profile-services" class="control-label col-lg-2">{{ __('lang.select_permissions') }}</label>
                                                 <div class="col-lg-10">
                                                     <ul id="treeview1">
-                                                        <th><input name="select_all" id="delete_all" type="checkbox" onclick="CheckAll('box1', this)" /></th>
+                                                        <th>
+                                                            <input name="select_all" id="delete_all" type="checkbox" onclick="CheckAll('box1', this)" />
+                                                        </th>
 
                                                         @foreach($permission as $value)
                                                             <br>
                                                             <label style="font-size: 16px;">
-                                                                <td><input id="cat-box" type="checkbox" name="permission[]" value="{{ $value->id }}" class="box1" @if (in_array($value->id, $rolePermissions)) checked @endif></td>
+                                                                <td>
+                                                                    <input id="cat-box" type="checkbox" @if (in_array($value->id, $rolePermissions)) checked @endif name="permission[]" value="{{ $value->id }}" class="box1">
+                                                                </td>
                                                                 {{ $value->name }}
                                                             </label>
                                                         @endforeach
@@ -56,7 +58,7 @@
                                         <hr />
                                         <div class="text-right">
                                             <button type="submit" class="btn btn-primary">
-                                                {{ __('roles.save') }}
+                                                {{ __('lang.save') }}
                                                 <i class="icon-arrow-left13 position-right"></i>
                                             </button>
                                         </div>
@@ -75,6 +77,11 @@
 
 @section('scripts')
 <script>
+    // Your existing scripts
+</script>
+@endsection
+
+<script>
     function CheckAll(className, elem) {
         var elements = document.getElementsByClassName(className);
         var l = elements.length;
@@ -89,4 +96,5 @@
         }
     }
 </script>
+
 @endsection
