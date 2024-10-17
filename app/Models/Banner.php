44013d664765416ec6eb40756da6aba154a9e49a
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2024-10-17 20:01:40 +0300

    test

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use App\Http\Resources\Core\MediaCenterResource;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Cron\ByClick;
 use App\Models\Cron\ByTime;
+use Spatie\Activitylog\LogOptions;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Translatable\HasTranslations;
 
+
 class Banner extends Model implements HasMedia
 {
+
 
     use HasTranslations;
     use InteractsWithMedia;
@@ -18,4 +22,20 @@ class Banner extends Model implements HasMedia
     public $translatable = ['title', 'description'];
     protected $guarded = ['id'];
     protected $table = "banners";
+
+
+    /*
+     * ----------------------------------------------------------------- *
+     * --------------------------- Acessores --------------------------- *
+     * ----------------------------------------------------------------- *
+     */
+
+
+    public function getImageUrlAttribute()
+    {
+        return asset(optional($this->getFirstMedia('banners'))->getUrl());
+    }
+
+
+
 }
